8dec85605392eea6298a94b44bac594fda17d26c
Sreejan Kumar <sreejan.kumar@yale.edu>
2016-11-17 18:20:44 -0500

    Digitizing script

---
+++ cleaning.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from pandas import DataFrame 
+
+
+def digitizeMatrix(raw_dataframe):
+    col_names=raw_dataframe.columns.values
+    for col_name in col_names:
+        if isinstance(raw_dataframe[col_name][0],str):
+            raw_dataframe[col_name]=pd.Categorical(raw_dataframe[col_name]).codes
+    
+
+print("Reading data...")
+pdtest = pd.read_csv('test_ver2.csv', delimiter = ',')
+#rawtraining=DataFrame.as_matrix(pdtest)
+print("Done!")
+digitizeMatrix(pdtest)
+print(pdtest)
+                    
+                    
