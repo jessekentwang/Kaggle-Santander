30cfead39478bbd3aed0572b56603a16dd8d6c4f
Sreejan Kumar <sk2436@tick.zoo.cs.yale.edu>
2016-12-02 18:16:38 -0500

    Subsetting and average precision

--- pipeline.py
+++ pipeline.py
@@ -1,53 +1,54 @@
 import pandas as pd
 import numpy as np
-from pandas import DataFrame 
+from pandas import DataFrame
 import pickle
 from sklearn.metrics import confusion_matrix
+import average_precision
 import os
 
 def prevDate(date, a):
-        if date.startswith('2015-01'):
-            return None
-        elif date.startswith('2016-01'):
-            return next((x for x in a if x.startswith('2015-12')), None)
-        else:
-            ltmp = date[:5]
-            tmp = (str(int(date[5:7]) - 1)).rjust(2, '0')
-            return next((x for x in a if x.startswith(ltmp+tmp)), None) 
+		if date.startswith('2015-01'):
+			return None
+		elif date.startswith('2016-01'):
+			return next((x for x in a if x.startswith('2015-12')), None)
+		else:
+			ltmp = date[:5]
+			tmp = (str(int(date[5:7]) - 1)).rjust(2, '0')
+			return next((x for x in a if x.startswith(ltmp+tmp)), None)
 
 def prevName(name):
-        if fits(name) or name == 'fecha_dato':
-            return name + '_prev'
-        else:
-            return name
+		if fits(name) or name == 'fecha_dato':
+			return name + '_prev'
+		else:
+			return name
 
 def addFeatures(data):
-        train = data[0]
-        test = data[1]
+		train = data[0]
+		test = data[1]
 
-        allData = pd.concat([train,test])
-        allData2 = allData.copy()
+		allData = pd.concat([train,test])
+		allData2 = allData.copy()
 
-        print ('Dataframes made!')
-        print (allData.head())
+		print ('Dataframes made!')
+		print (allData.head())
 
-        allData2 = allData2.rename(columns = lambda x: prevName(x), inplace = True)
+		allData2 = allData2.rename(columns = lambda x: prevName(x), inplace = True)
 
-        print ('DF2 renamed!')
-        print (allData.head())
+		print ('DF2 renamed!')
+		print (allData.head())
 
-        a = set(allData['fecha_dato'])
-        allData['fecha_dato_prev'] = allData['fecha_dato'].apply(lambda x: prevDate(x, a))
+		a = set(allData['fecha_dato'])
+		allData['fecha_dato_prev'] = allData['fecha_dato'].apply(lambda x: prevDate(x, a))
 
-        print ('DF1 dates readjusted')
-        print ('Now merging...')
+		print ('DF1 dates readjusted')
+		print ('Now merging...')
 
-        allData = pd.merge(allData, allData2, how = 'left', on = ['fecha_dato_prev', 'ncodpers'])
+		allData = pd.merge(allData, allData2, how = 'left', on = ['fecha_dato_prev', 'ncodpers'])
 
-        return allData
+		return allData
 
 def cleanTrain(n = None):
-	
+
 	print("Starting Cleaning Script!\n")
 
 	if os.path.isfile('RawTrain.pickle'):
@@ -81,8 +82,8 @@ def cleanTrain(n = None):
 			if stillBad[i] > 0:
 				pdtrain = pdtrain.drop(stillBad.index[i], 1)
 				pdtest = pdtest.drop(stillBad.index[i], 1)
-	
-	print ("Data Clean!")  
+
+	print ("Data Clean!")
 
 	print ("Writing Data...")
 
@@ -90,13 +91,13 @@ def cleanTrain(n = None):
 		pickle.dump(pdtrain, open(r'RawTrain.pickle', "wb"))
 		pickle.dump(pdtest, open(r'RawTest.pickle', 'wb'))
 
-	print ("pdtrain shape: ", pdtrain.shape)  
+	print ("pdtrain shape: ", pdtrain.shape)
 	print ("pdtest shape: ", pdtest.shape)
-		
+
 	print ("Cleaning script done!\n")
 
-	
-	
+
+
 	return [pdtrain, pdtest]
 
 def fits(c):
@@ -119,7 +120,7 @@ def digitizeMatrix(raw_dataframe):
 		if isinstance(raw_dataframe[col_name][0],str):
 			raw_dataframe[col_name]=pd.Categorical(raw_dataframe[col_name]).codes
 
-def gen_classify(reg,trainFeatures,trainTarget):
+def gen_classify_cv(reg,trainFeatures,trainTarget):
 	sz=int(len(trainFeatures)/2)
 	cvFeatures = trainFeatures[sz:]
 	trainFeatures = trainFeatures[:sz]
@@ -137,8 +138,8 @@ def gen_classify(reg,trainFeatures,trainTarget):
 		All_Targets.append(cvTarget)
 
 		Target = trainTarget[target1][:sz]
-                print(len(Target))
-                print(len(trainFeatures))
+		print(len(Target))
+		print(len(trainFeatures))
 		#reg = model_method(class_weight = 'balanced')
 		reg.fit(trainFeatures, Target)
 
@@ -150,9 +151,34 @@ def gen_classify(reg,trainFeatures,trainTarget):
 		print (conf[i])
 		#print (classification_report(cvTarget, predictions[i]))
 		print ('True positive rate is: ' + str((conf[i][1][1])/(conf[i][1][0] + conf[i][1][1])))
+		print("Accuracy is: "+str((conf[i][0][0]+conf[i][1][1])/(conf[i][0][0]+conf[i][0][1]+conf[i][1][0]+conf[i][1][1])))
 		print ('--------')
 
-	return [predictions, cvTarget]
+	return [predictions, All_Targets]
+
+def gen_classify_test(reg,trainFeatures,trainTarget,month):
+	trainingData=trainFeatures[trainFeatures.fecha_dato==month]
+	trainingLabels=trainTarget[trainFeatures.fecha_dato==(month)]
+	testingData=trainFeatures[trainFeatures.fecha_dato==(month+1)]
+	testingLabels=trainTarget[trainFeatures.fecha_dato==(month+1)]
+	predictions=[]
+	conf=[]
+
+	N=len(testingLabels.columns)
+	for i in range(0,N):
+		t1=(trainingLabels.columns[i])
+		reg.fit(trainingData,trainingLabels[t1])
+		predictions.append(reg.predict(testingData))
+		conf.append(confusion_matrix(testingLabels[t1],predictions[i]))
+
+		print(t1)
+		print(conf[i])
+		print ('True positive rate is: ' + str((conf[i][1][1])/(conf[i][1][0] + conf[i][1][1])))
+		print("Accuracy is: "+str((conf[i][0][0]+conf[i][1][1])/(conf[i][0][0]+conf[i][0][1]+conf[i][1][0]+conf[i][1][1])))
+		print ('--------')
+	print("AVERAGE PRECISION: ")
+	print(average_precision.mapk(np.asarray(testingLabels),np.asarray(predictions)))
+	return [predictions,conf]
 
 def load_data():
 	train, test=cleanTrain()
@@ -162,6 +188,5 @@ def load_data():
 	return(trainFeatures,trainTarget,test)
 
 if __name__ == "__main__":
-        a = cleanTrain()
-        print (a[0].head())
-
+		a = cleanTrain()
+		print (a[0].head())
